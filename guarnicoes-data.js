e8289aeb5e2a9e7199912df1e835c3950539401d
ciabm-1bbm <gustavo.medeiros@outlook.com>
2025-06-28 22:18:15 -0300

    Update guarnicoes-data.js

--- guarnicoes-data.js
+++ guarnicoes-data.js
@@ -1,13 +1,13 @@
 // guarnicoes-data.js
 // ATENÇÃO: ESTE ARQUIVO É GERADO AUTOMATICAMENTE E SUBSTITUIRÁ QUALQUER EDIÇÃO MANUAL
-// A linha 'export' foi adicionada para compatibilidade com módulos JS
+// AS PALAVRAS 'export' FORAM REMOVIDAS PARA COMPATIBILIDADE SEM MODULOS ES
 
-export const DATA_GUARNA = "28/06/2025"; // Data atualizada para o dia de hoje
+const DATA_GUARNA = "28/06/2025"; // Data atualizada para o dia de hoje
 
-export const GUARNICOES = {
+const GUARNICOES = {
     // ESTE É O BLOCO DE DADOS DAS GUARNIÇÕES QUE VOCÊ COLO DA ÚLTIMA VEZ.
     // DEVE ESTAR AQUI COMPLETAMENTE COM TODAS AS 1ª CIA, 2ª CIA, NAO CLASSIFICADO.
-    // Exemplo de como começa:
+    // O conteúdo que eu te dei anteriormente para guarnicoes-data.js deve estar aqui.
     "1ª CIA": {
         "BBS - CANIL": {
             "colorClass": "bg-gray-600",
@@ -23,18 +23,394 @@ export const GUARNICOES = {
                 }
             ]
         },
-        // ... (todo o resto dos seus dados de guarnições) ...
+        "BRAVO 12 - TERESÓPOLIS": {
+            "colorClass": "bg-color-bravo12",
+            "viaturas": [
+                {
+                    "tipo": "ABT",
+                    "prefixo": "0594",
+                    "turno": "24h",
+                    "guarnicao": [
+                        { "funcao": "CMT DE GU", "nomeGuerra": "SD CASSIANO" },
+                        { "funcao": "COV", "nomeGuerra": "SD GONÇALVES" }
+                    ]
+                },
+                {
+                    "tipo": "ABT",
+                    "prefixo": "0594",
+                    "turno": "12h - dia (08h-20h)",
+                    "guarnicao": [
+                        { "funcao": "COMBATENTE 1", "nomeGuerra": "SD TOMIELLO" }
+                    ]
+                },
+                {
+                    "tipo": "ABT",
+                    "prefixo": "0594",
+                    "turno": "12h - noite (20h-08h)",
+                    "guarnicao": [
+                        { "funcao": "COMBATENTE 1", "nomeGuerra": "SD ROSEANNE" }
+                    ]
+                },
+                {
+                    "tipo": "AR",
+                    "prefixo": "0766",
+                    "turno": "24h",
+                    "guarnicao": [
+                        { "funcao": "CMT DE GU", "nomeGuerra": "SD DORNELES" },
+                        { "funcao": "COV", "nomeGuerra": "SD TEMP AMARANTE" }
+                    ]
+                },
+                 {
+                    "tipo": "AR",
+                    "prefixo": "0766",
+                    "turno": "12h - dia (08h-20h)",
+                    "guarnicao": [
+                        { "funcao": "SOCORRISTA 2", "nomeGuerra": "SD ROSEANNE" }
+                    ]
+                }
+            ]
+        },
+        "BRAVO 1 - AÇORIANOS": {
+            "colorClass": "bg-color-bravo1",
+            "viaturas": [
+                {
+                    "tipo": "ABT",
+                    "prefixo": "0595",
+                    "turno": "24h",
+                    "guarnicao": [
+                        { "funcao": "CMT DE GU", "nomeGuerra": "1º SGT FLÁVIO" },
+                        { "funcao": "COV", "nomeGuerra": "2º SGT GUIMARÃES" },
+                        { "funcao": "COMBATENTE 1", "nomeGuerra": "SD MAURICIO" }
+                    ]
+                },
+                {
+                    "tipo": "AEM",
+                    "prefixo": "0482",
+                    "turno": "12h (07h-19h)",
+                    "guarnicao": [
+                        { "funcao": "COV", "nomeGuerra": "2º SGT BECKER" }
+                    ]
+                },
+                {
+                    "tipo": "AEM",
+                    "prefixo": "0482",
+                    "turno": "24h",
+                    "guarnicao": [
+                        { "funcao": "COV", "nomeGuerra": "SD BOTEGA" }
+                    ]
+                },
+                {
+                    "tipo": "AT",
+                    "prefixo": "0672",
+                    "turno": "12h (07h-19h)",
+                    "guarnicao": [
+                        { "funcao": "COV", "nomeGuerra": "2º SGT BECKER" }
+                    ]
+                },
+                {
+                    "tipo": "AT",
+                    "prefixo": "0672",
+                    "turno": "24h",
+                    "guarnicao": [
+                        { "funcao": "COV", "nomeGuerra": "SD BOTEGA" }
+                    ]
+                },
+                {
+                    "tipo": "ATP",
+                    "prefixo": "201",
+                    "turno": "24h",
+                    "guarnicao": [
+                        { "funcao": "COV", "nomeGuerra": "SD PESSOTA" }
+                    ]
+                }
+            ]
+        },
+        "BRAVO 6 - ASSUNÇÃO": {
+            "colorClass": "bg-color-bravo6",
+            "viaturas": [
+                {
+                    "tipo": "ABT",
+                    "prefixo": "0596",
+                    "turno": "24h",
+                    "guarnicao": [
+                        { "funcao": "CMT DE GU", "nomeGuerra": "SD LEANDRO" },
+                        { "funcao": "COMBATENTE 1", "nomeGuerra": "SD PORCELIS" }
+                    ]
+                },
+                {
+                    "tipo": "ABT",
+                    "prefixo": "0596",
+                    "turno": "12h - dia (08h-20h)",
+                    "guarnicao": [
+                        { "funcao": "COV", "nomeGuerra": "SD RAUBACH" }
+                    ]
+                },
+                {
+                    "tipo": "ABT",
+                    "prefixo": "0596",
+                    "turno": "12h - noite (20h-08h)",
+                    "guarnicao": [
+                        { "funcao": "COV", "nomeGuerra": "2º SGT PANAZZOLO" }
+                    ]
+                }
+            ]
+        },
+        "BRAVO 11 - RESTINGA": {
+            "colorClass": "bg-color-bravo11",
+            "viaturas": [
+                {
+                    "tipo": "ABT",
+                    "prefixo": "0598",
+                    "turno": "24h",
+                    "guarnicao": [
+                        { "funcao": "CMT DE GU", "nomeGuerra": "SD BRUNO" },
+                        { "funcao": "COMBATENTE 1", "nomeGuerra": "SD CATELAN" },
+                        { "funcao": "COV", "nomeGuerra": "SD MAURÍCIO" }
+                    ]
+                }
+            ]
+        },
+        "BRAVO 10 - BELÉM NOVO": {
+            "colorClass": "bg-color-bravo10",
+            "viaturas": [
+                {
+                    "tipo": "ABT",
+                    "prefixo": "0599",
+                    "turno": "24h",
+                    "guarnicao": [
+                        { "funcao": "CMT DE GU", "nomeGuerra": "1º SGT PME JULIANO" },
+                        { "funcao": "COMBATENTE 1", "nomeGuerra": "SD RAFAEL" },
+                        { "funcao": "COV", "nomeGuerra": "SD ROGER" }
+                    ]
+                }
+            ]
+        },
+        "BBS - TERRESTRE": {
+            "colorClass": "bg-gray-600",
+            "viaturas": [
+                {
+                    "tipo": "ABTS",
+                    "prefixo": "866",
+                    "turno": "12h - dia (08h-20h)",
+                    "guarnicao": [
+                        { "funcao": "CMT DE GU", "nomeGuerra": "2º SGT DARLAN" },
+                        { "funcao": "SOCORRISTA 2", "nomeGuerra": "SD SELAU" }
+                    ]
+                },
+                {
+                    "tipo": "ABTS",
+                    "prefixo": "866",
+                    "turno": "24h",
+                    "guarnicao": [
+                        { "funcao": "SOCORRISTA 1", "nomeGuerra": "SD MARQUES" },
+                        { "funcao": "COV", "nomeGuerra": "SD ORNELAS" }
+                    ]
+                },
+                {
+                    "tipo": "ABTS",
+                    "prefixo": "866",
+                    "turno": "12h - noite (20h-08h)",
+                    "guarnicao": [
+                        { "funcao": "CMT DE GU", "nomeGuerra": "2º SGT MÜLLER" },
+                        { "funcao": "SOCORRISTA 2", "nomeGuerra": "AL SD LEMOS" }
+                    ]
+                }
+            ]
+        }
     },
-    // ... (fechamento do objeto GUARNICOES) ...
+    "2ª CIA": {
+        "BRAVO 2 - FLORESTA": {
+            "colorClass": "bg-color-bravo2",
+            "viaturas": [
+                {
+                    "tipo": "ABT",
+                    "prefixo": "0597",
+                    "turno": "2h (08:00)",
+                    "guarnicao": [
+                        { "funcao": "CMT DE GU", "nomeGuerra": "1º SGT GUEDES" },
+                        { "funcao": "COMBATENTE 1", "nomeGuerra": "2º SGT SAUL" },
+                        { "funcao": "COMBATENTE 3", "nomeGuerra": "SD CRISTIANO" }
+                    ]
+                },
+                {
+                    "tipo": "ABT",
+                    "prefixo": "0597",
+                    "turno": "24h",
+                    "guarnicao": [
+                        { "funcao": "COV", "nomeGuerra": "SD TEMP DOTTA" }
+                    ]
+                },
+                {
+                    "tipo": "ABT",
+                    "prefixo": "0597",
+                    "turno": "2h (10:00)",
+                    "guarnicao": [
+                        { "funcao": "CMT DE GU", "nomeGuerra": "2º SGT SAUL" },
+                        { "funcao": "COMBATENTE 1", "nomeGuerra": "SD CRISTIANO" }
+                    ]
+                }
+            ]
+        },
+        "BRAVO 3 - PASSO D'AREIA": {
+            "colorClass": "bg-color-bravo3",
+            "viaturas": [
+                {
+                    "tipo": "ABT",
+                    "prefixo": "0600",
+                    "turno": "24h",
+                    "guarnicao": [
+                        { "funcao": "COV", "nomeGuerra": "SD BARBOSA" },
+                        { "funcao": "COMBATENTE 2", "nomeGuerra": "SD MOURA" },
+                        { "funcao": "CMT DE GU", "nomeGuerra": "SD THALIN" }
+                    ]
+                }
+            ]
+        },
+        "BRAVO 7 - PARTENON": {
+            "colorClass": "bg-color-bravo7",
+            "viaturas": [
+                {
+                    "tipo": "ABT",
+                    "prefixo": "327",
+                    "turno": "24h",
+                    "guarnicao": [
+                        { "funcao": "COMBATENTE 1", "nomeGuerra": "1º SGT ARIAS" },
+                        { "funcao": "CMT DE GU", "nomeGuerra": "1º SGT SOARES" },
+                        { "funcao": "COV", "nomeGuerra": "3º SGT RAMOS" },
+                        { "funcao": "COMBATENTE 3", "nomeGuerra": "SD JONATHAN" }
+                    ]
+                }
+            ]
+        },
+        "BBS - MERGULHO": {
+            "colorClass": "bg-gray-600",
+            "viaturas": [
+                {
+                    "tipo": "ATP",
+                    "prefixo": "0555",
+                    "turno": "24h",
+                    "guarnicao": [
+                        { "funcao": "CMT DE GU", "nomeGuerra": "1º SGT VILELA" },
+                        { "funcao": "MERGULHADOR", "nomeGuerra": "SD ANDERSON" },
+                        { "funcao": "MERGULHADOR", "nomeGuerra": "SD LOURIVAL" },
+                        { "funcao": "MERGULHADOR", "nomeGuerra": "SD STACHACK" }
+                    ]
+                }
+            ]
+        },
+        "BBS - AQUÁTICA": {
+            "colorClass": "bg-gray-600",
+            "viaturas": [
+                {
+                    "tipo": "EAL",
+                    "prefixo": "716",
+                    "turno": "24h",
+                    "guarnicao": [
+                        { "funcao": "CMT DE GU", "nomeGuerra": "2º SGT CRISTIANO DA ROSA DE SOUZA" },
+                        { "funcao": "AUX. CONDUTOR NAVAL 2", "nomeGuerra": "SD ALBERTO" },
+                        { "funcao": "AUX. CONDUTOR NAVAL 1", "nomeGuerra": "SD AURÉLIO" },
+                        { "funcao": "CONDUTOR NAVAL", "nomeGuerra": "SD UMPIERRE" }
+                    ]
+                },
+                {
+                    "tipo": "EIN",
+                    "prefixo": "713",
+                    "turno": "24h",
+                    "guarnicao": [
+                        { "funcao": "CMT DE GU", "nomeGuerra": "2º SGT CRISTIANO DA ROSA DE SOUZA" },
+                        { "funcao": "AUX. CONDUTOR NAVAL 2", "nomeGuerra": "SD ALBERTO" },
+                        { "funcao": "AUX. CONDUTOR NAVAL 1", "nomeGuerra": "SD AURÉLIO" },
+                        { "funcao": "CONDUTOR NAVAL", "nomeGuerra": "SD UMPIERRE" }
+                    ]
+                },
+                {
+                    "tipo": "EIN",
+                    "prefixo": "714",
+                    "turno": "24h",
+                    "guarnicao": [
+                        { "funcao": "CMT DE GU", "nomeGuerra": "2º SGT CRISTIANO DA ROSA DE SOUZA" },
+                        { "funcao": "AUX. CONDUTOR NAVAL 2", "nomeGuerra": "SD ALBERTO" },
+                        { "funcao": "AUX. CONDUTOR NAVAL 1", "nomeGuerra": "SD AURÉLIO" },
+                        { "funcao": "CONDUTOR NAVAL", "nomeGuerra": "SD UMPIERRE" }
+                    ]
+                },
+                {
+                    "tipo": "EMN",
+                    "prefixo": "943",
+                    "turno": "24h",
+                    "guarnicao": [
+                        { "funcao": "CMT DE GU", "nomeGuerra": "2º SGT CRISTIANO DA ROSA DE SOUZA" },
+                        { "funcao": "AUX. CONDUTOR NAVAL 2", "nomeGuerra": "SD ALBERTO" },
+                        { "funcao": "AUX. CONDUTOR NAVAL 1", "nomeGuerra": "SD AURÉLIO" },
+                        { "funcao": "CONDUTOR NAVAL", "nomeGuerra": "SD UMPIERRE" }
+                    ]
+                },
+                {
+                    "tipo": "EMN",
+                    "prefixo": "948",
+                    "turno": "24h",
+                    "guarnicao": [
+                        { "funcao": "CMT DE GU", "nomeGuerra": "2º SGT CRISTIANO DA ROSA DE SOUZA" },
+                        { "funcao": "AUX. CONDUTOR NAVAL 2", "nomeGuerra": "SD ALBERTO" },
+                        { "funcao": "AUX. CONDUTOR NAVAL 1", "nomeGuerra": "SD AURÉLIO" },
+                        { "funcao": "CONDUTOR NAVAL", "nomeGuerra": "SD UMPIERRE" }
+                    ]
+                }
+            ]
+        }
+    },
+    "NAO CLASSIFICADO": {
+        "AODCA": {
+            "colorClass": "bg-gray-600",
+            "viaturas": [
+                {
+                    "tipo": "ATP",
+                    "prefixo": "765",
+                    "turno": "24h",
+                    "guarnicao": [
+                        { "funcao": "COV", "nomeGuerra": "1º TEN PME JAIRO" },
+                        { "funcao": "OFICIAL SUPERVISOR", "nomeGuerra": "CAP PELLEGRINI" }
+                    ]
+                }
+            ]
+        },
+        "COBOM-DCCI": {
+            "colorClass": "bg-gray-600",
+            "viaturas": [
+                {
+                    "tipo": "DCCI",
+                    "prefixo": "NAO CLASSIFICADO",
+                    "turno": "12h (07h-19h)",
+                    "guarnicao": [
+                        { "funcao": "OPERADOR COBOM", "nomeGuerra": "1º SGT RIBEIRO" },
+                        { "funcao": "OPERADOR COBOM", "nomeGuerra": "2º SGT BARBARA" }
+                    ]
+                },
+                {
+                    "tipo": "DCCI",
+                    "prefixo": "NAO CLASSIFICADO",
+                    "turno": "12h (19h-07h)",
+                    "guarnicao": [
+                        { "funcao": "OPERADOR COBOM", "nomeGuerra": "1º TEN PME ELEAZAR" },
+                        { "funcao": "OPERADOR COBOM", "nomeGuerra": "2º SGT CESAR" }
+                    ]
+                }
+            ]
+        },
+        "1º BBM / ESTADO MAIOR": {
+            "colorClass": "bg-gray-600",
+            "viaturas": []
+        }
+    }
 };
 
 // Funções para obter o ícone da viatura
-export function getViaturaIcon(tipoViatura) {
+function getViaturaIcon(tipoViatura) {
     switch (tipoViatura.toUpperCase()) {
-        case 'ABT': return 'fas fa-truck-fire';
-        case 'AR': return 'fas fa-ambulance';
-        case 'AEM': return 'fas fa-truck-ladder';
-        case 'ATP': return 'fas fa-car-side';
+        case 'ABT': return 'fas fa-truck-fire'; // Caminhão de bombeiro (Fire Truck)
+        case 'AR': return 'fas fa-ambulance'; // Ambulância
+        case 'AEM': return 'fas fa-truck-ladder'; // Caminhão com escada (Ladder Truck)
+        case 'ATP': return 'fas fa-car-side'; // Carro normal (ou pick-up se preferir 'fas fa-truck-pickup')
         case 'ABC': return 'fas fa-dog';
         case 'AT': return 'fas fa-truck-pickup';
         case 'ABTS': return 'fas fa-hand-holding-medical';
@@ -49,17 +425,17 @@ export function getViaturaIcon(tipoViatura) {
 }
 
 // Funções para obter o ícone do militar baseado na função
-export function getMilitarIcon(funcao) {
+function getMilitarIcon(funcao) {
     switch (funcao.toUpperCase()) {
-        case 'CMT DE GU': return 'fas fa-user-tie';
-        case 'COV': return 'fas fa-helmet-safety';
+        case 'CMT DE GU': return 'fas fa-user-tie'; // Gravata (líder)
+        case 'COV': return 'fas fa-helmet-safety'; // Capacete
         case 'COMBATENTE 1':
         case 'COMBATENTE 2':
-        case 'COMBATENTE 3': return 'fas fa-user-firefighter';
-        case 'MOTORISTA': return 'fas fa-steering-wheel';
+        case 'COMBATENTE 3': return 'fas fa-user-firefighter'; // Bombeiro (Font Awesome 6)
+        case 'MOTORISTA': return 'fas fa-steering-wheel'; // Volante
         case 'SOCORRISTA 1':
-        case 'SOCORRISTA 2': return 'fas fa-medkit';
-        case 'ALFA 4': return 'fas fa-id-badge';
+        case 'SOCORRISTA 2': return 'fas fa-medkit'; // Kit médico
+        case 'ALFA 4': return 'fas fa-id-badge'; // Crachá
         case 'OPERADOR COBOM': return 'fas fa-headset';
         case 'OFICIAL SUPERVISOR': return 'fas fa-user-shield';
         case 'MERGULHADOR': return 'fas fa-diving-mask';
