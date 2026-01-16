f8539debedb8ce8330908edc59ab3b6adee119f6
ciabm-1bbm <gustavo.medeiros@outlook.com>
2026-01-16 02:18:51 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,12 @@
+const cacheName = 'cia-1bbm-v1';
+const assets = ['./', './index.html'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
